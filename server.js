616a9c80d4a81e1111f914c2fc1e30546827c007
srv89 <sarveshsadhoo@gmail.com>
2016-02-16 16:55:57 -0800

    added socket.io on clien/server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var morgan = require('morgan');
+
 var app = express();
 var http = require('http').Server(app);
-var morgan = require('morgan');
+var io = require('socket.io')(http);
+
 
 var PORT = process.env.PORT || 3000;
 var ENV = process.env.NODE_ENV || 'development';
@@ -12,6 +15,10 @@ app.use(express.static(__dirname + '/public'));
 if (ENV === 'development') {
 	app.use(morgan('common'));
 }
+
+io.on('connection', function () {
+	console.log('User connected via socket.io!');
+});
 
 
 http.listen(PORT, function () {
