7948698ca7cdb945e62689f0e4682d5931002fef
srv89 <sarveshsadhoo@gmail.com>
2016-02-16 16:23:15 -0800

    Added morgan

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var PORT = process.env.PORT || 3000;
 var express = require('express');
-var http = require('http').Server(app);
 var app = express();
+var http = require('http').Server(app);
+var morgan = require('morgan');
+
+var PORT = process.env.PORT || 3000;
+var ENV = process.env.NODE_ENV || 'development';
+
 
 app.use(express.static(__dirname + '/public'));
 
+if (ENV === 'development') {
+	app.use(morgan('common'));
+}
+
+
 http.listen(PORT, function () {
 	console.log('Express listening on PORT ' + PORT + '!');
-})
+});
