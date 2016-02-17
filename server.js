ca614f2c6db3c812c4c8b30cb8a7ba7e68483b41
srv89 <sarveshsadhoo@gmail.com>
2016-02-16 16:14:53 -0800

    Added simple server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-
-
 var PORT = process.env.PORT || 3000;
-
+var express = require('express');
+var http = require('http').Server(app);
 var app = express();
 
-app.listen(PORT function () {
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function () {
 	console.log('Express listening on PORT ' + PORT + '!');
 })
