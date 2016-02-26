58347ffcd5d4ff9f155188f2558af304cbf580d9
srv89 <sarveshsadhoo@gmail.com>
2016-02-26 14:22:41 -0800

    added momentjs examples

---
+++ moment-example.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+
+var now = moment()
+
+/*console.log(now.format('MMMM Do YYYY, h:mm:ss a'));
+console.log(now.format('MMM Do YYYY, h:mma'));
+
+console.log(now.format('X'));
+console.log(now.format('x'));
+
+console.log(now.valueOf());*/
+
+
+var timeStamp = 1456522732489;
+var timeStampMoment = moment.utc(timeStamp);
+//var localTime = timeStampMoment.local()
+
+console.log(timeStampMoment.local().format('h:mm a'));
