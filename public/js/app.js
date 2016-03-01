2f1ac1b26977de4398549b737c9ccc67a4e6999c
srv89 <sarveshsadhoo@gmail.com>
2016-02-29 16:17:40 -0800

    added user disconnect

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-var socket = io();
-
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
+var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+// Update h1 tag
 jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
@@ -26,19 +26,18 @@ socket.on('message', function (message) {
 	$message.append('<p>' + message.text + '</p>');
 });
 
-// Handles sumitting of new message
-
+// Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', function (event){
+$form.on('submit', function (event) {
 	event.preventDefault();
 
-	var $message = $form.find('input[name=message]')
-	
+	var $message = $form.find('input[name=message]');
+
 	socket.emit('message', {
 		name: name,
 		text: $message.val()
 	});
 
 	$message.val('');
-})
+});
--- server.js
+++ server.js
@@ -1,54 +1,57 @@
+var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
-var morgan = require('morgan');
-
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
-
-var PORT = process.env.PORT || 3000;
-var ENV = process.env.NODE_ENV || 'development';
-
 
 app.use(express.static(__dirname + '/public'));
-
-
-if (ENV === 'development') {
-	app.use(morgan('common'));
-}
 
 var clientInfo = {};
 
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
 		socket.broadcast.to(req.room).emit('message', {
 			name: 'System',
-			text: req.name + ' has joined!', 
+			text: req.name + ' has joined!',
 			timestamp: moment().valueOf()
-		})
+		});
 	});
 
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
-		message.timestamp = moment.valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
 
+		message.timestamp = moment().valueOf();
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
+	// timestamp property - JavaScript timestamp (milliseconds)
 
 	socket.emit('message', {
 		name: 'System',
 		text: 'Welcome to the chat application!',
-		timestamp: moment.valueOf()
+		timestamp: moment().valueOf()
 	});
 });
 
-
-
 http.listen(PORT, function () {
-	console.log('Express listening on PORT ' + PORT + '!');
+	console.log('Server started!');
 });
