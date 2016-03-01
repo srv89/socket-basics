0a25635122c3a20cb785ca4baecb0784078f26dd
srv89 <sarveshsadhoo@gmail.com>
2016-02-29 17:08:34 -0800

    added @currentUsers to list all users

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	};
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		};
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment.valueOf()
+	});
+}
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
@@ -39,8 +62,14 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
+
+		
 	});
 
 	// timestamp property - JavaScript timestamp (milliseconds)
